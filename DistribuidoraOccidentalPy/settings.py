7af5a86d128bb2f6e47d1b52bfae4bfb9f05bc33
Gian Paul <99435962+gianberbesi@users.noreply.github.com>
2023-03-01 02:09:59 -0500

    :)

--- DistribuidoraOccidentalPy/settings.py
+++ DistribuidoraOccidentalPy/settings.py
@@ -169,7 +169,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-co'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Bogota'
 
