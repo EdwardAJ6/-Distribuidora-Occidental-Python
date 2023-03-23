3dbccbe2777066dd6bd54369f1dfb65e2156c3e2
EdwardAJ6 <eialba0@misena.edu.co>
2023-03-22 23:08:44 -0500

    Recuperar Contraseña

--- DistribuidoraOccidentalPy/urls.py
+++ DistribuidoraOccidentalPy/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 from django.conf.urls.static import static
@@ -31,6 +32,16 @@ urlpatterns = [
     path('inventario/', include('inventario.urls')),
     path('correo/', include('correo.urls')),
     path('buscar/', views.buscar_productos, name='buscar'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_send/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password_complete/', auth_views.PasswordChangeDoneView.as_view(), name='password_reset_complete'),
+    path('accounts/login/',views.login_view, name='accounts_login'),
+
+
+
+
 
 
 
